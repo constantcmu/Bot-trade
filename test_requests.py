2fca75417c3ed55bd52f9caf5893962645e8c9a1
constantcmu <constant.eng.cmu@gmail.com>
2022-01-15 14:52:04 +0700

    fix ...

--- test_requests.py
+++ test_requests.py
@@ -2,6 +2,7 @@ import json
 import requests
 
 url = 'http://127.0.0.1:5000/signals'
+heroku_url = "https://immense-retreat-47353.herokuapp.com/signals"
 msg = {
             'ACTION': 'OPEN LONG',
             'AMOUNT_COIN': '0.01',
@@ -11,6 +12,6 @@ msg = {
 
 msg = json.dumps(msg)
 
-x = requests.post(url, data = msg)
+x = requests.post(heroku_url, data = msg)
 
 print(x.text)
