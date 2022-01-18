ecc67798ed2ad84444de4cdef419afaba33abe5a
constantcmu <constant.eng.cmu@gmail.com>
2022-01-18 11:45:20 +0700

    add

--- test_requests.py
+++ test_requests.py
@@ -5,10 +5,10 @@ import requests
 url = 'http://127.0.0.1:5000/signals'
 heroku_url = "https://immense-retreat-47353.herokuapp.com/signals"
 msg = {
-            'ACTION': 'OPEN LONG',
-            'AMOUNT_COIN': '0.01',
-            'LEV': '30',
-            'SYMBOL': 'BTCBUSD',
+            'ACTION': 'TPSL LONG',
+            'AMOUNT_COIN': '0.04',
+            'LEV': '0',
+            'SYMBOL': 'BNBBUSD',
         }
 
 msg = json.dumps(msg)
